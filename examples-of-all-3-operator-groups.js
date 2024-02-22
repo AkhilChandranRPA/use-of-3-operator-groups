cb17ee3a25bc6890bfddc43c832237217642facb
AkhilChandranRPA <160742604+AkhilChandranRPA@users.noreply.github.com>
2024-02-22 11:13:02 +0000

    examples of all 3 operator groups

---
+++ examples-of-all-3-operator-groups.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 3;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+
+console.log("(a + b) =", sum);
+console.log("(a - b) =", difference);
+console.log("(a * b) =", product);
+console.log("(a / b) =", quotient);
+console.log("(a % b) =", remainder);
+
+// Comparison Operators
+a = 5;
+b = 10;
+
+let isEqual = (a == b);
+let isNotEqual = (a != b);
+let isGreater = (a > b);
+let isLess = (a < b);
+let isGreaterOrEqual = (a >= b);
+let isLessOrEqual = (a <= b);
+
+console.log("(a == b) =", isEqual);
+console.log("(a != b) =", isNotEqual);
+console.log("(a > b)  =", isGreater);
+console.log("(a < b)  =", isLess);
+console.log("(a >= b) =", isGreaterOrEqual);
+console.log("(a <= b) =", isLessOrEqual);
+
+// Logical Operators:
+let x = true;
+let y = false;
+
+let andResult = x && y;
+let orResult = x || y;
+let notResult = !x;
+
+console.log("(x && y) =", andResult);
+console.log("(x || y) =", orResult);
+console.log("(!x)      =", notResult);
